a659f31e5e402ba1ca26a40fd5802c2bac144e86
jordynmcginnis <jordynm4@gmail.com>
2019-01-16 12:46:43 -0700

    fixed indention

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ class App extends Component {
       <div className="App">
           <div className='home-container'>
             <div className='options'>
-              <a href='https://github.com/jordynMcginnis' className='hi'>Github</a>
-              <a href='https://github.com/jordynMcginnis/emoji-game' className='hi'>Twitter</a>
-              <a href='https://www.linkedin.com/in/jordyn-mcginnis-51a729150/' className='hi'>LinkedIn</a>
+              <a href='https://github.com/jordynMcginnis'>Github</a>
+              <a href='https://github.com/jordynMcginnis/emoji-game'>Twitter</a>
+              <a href='https://www.linkedin.com/in/jordyn-mcginnis-51a729150/'>LinkedIn</a>
             </div>
             <div className='name-title'>
-              <div className='name' >JORDYN MCGINNIS</div>
+              <div className='name'>JORDYN MCGINNIS</div>
               <div className='email'>jordynbmcginnis@gmail.com</div>
             </div>
-            <div className='my-photo'>
-            </div>
+            <div className='my-photo'></div>
           </div>
-
       </div>
     );
   }
