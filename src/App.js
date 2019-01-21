1b9ede02d8a413a4e754241c1434d981ca0a2278
jordynmcginnis <jordynm4@gmail.com>
2019-01-21 09:58:02 -0700

    added react-icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FaGithub } from 'react-icons/fa';
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
               <div className='email'>jordynbmcginnis@gmail.com</div>
               <div className='options'>
                 <a href='https://github.com/jordynMcginnis'>Github</a>
-                <a href='https://github.com/jordynMcginnis/emoji-game'>Twitter</a>
+                <a href='https:twitter.com/jordynbmcginnis'>Twitter</a>
                 <a href='https://www.linkedin.com/in/jordyn-mcginnis-51a729150/'>LinkedIn</a>
               </div>
             </div>
